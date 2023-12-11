cc8906804c150f606092172614ec99b349a9c49f
MinchinWeb <w_minchin@hotmail.com>
2023-12-10 22:28:01 -0700

    Process front matter as YAML

---
+++ minchin/pelican/readers/commonmark/pre_process.py
@@ -0,0 +1,182 @@
+"""
+Functionality run before the Markdown render is run.
+
+- pull front matter from Source file.
+- take tag only lines and add them to metadata and remove them from the body
+"""
+from datetime import datetime
+import logging
+
+import yaml
+
+from pelican.utils import SafeDatetime
+
+from .constants import LOG_PREFIX
+
+logger = logging.getLogger(__name__)
+
+
+def read_front_matter(self, raw_text, metadata, md):
+    """
+    Read front matter and split from main body text.
+
+    Args:
+        self (pelican.reader.BaseReader): used to pull settings, and
+        process_metadata() function.
+        raw_text (str): Raw, unprocessed source text (likely in raw Markdown)
+        md (markdown_it.MarkdownIT): Markdown renderer
+
+    Returns:
+        content (str): Raw source text, now without metadata
+        metadata (dict): Metadata of the source text.
+    """
+    formatted_fields = self.settings["FORMATTED_FIELDS"]
+
+    # check if we have the front-matter extension (aka plugin) active
+    _front_matter_extension_active = False
+    if "extensions" in self.settings["COMMONMARK"]:
+        for my_extension in self.settings["COMMONMARK"]["extensions"]:
+            if my_extension.__name__ == "front_matter_plugin":
+                _front_matter_extension_active = True
+                break
+
+    front_matter_text = ""
+    if _front_matter_extension_active:
+        # TODO: pull front matter and body without having to render the
+        # Markdown twice
+        md_tokens = md.parse(raw_text)
+        for my_token in md_tokens:
+            if my_token.type == "front_matter":
+                front_matter_text = my_token.content
+                front_matter_text = front_matter_text.splitlines()
+                # assumes a single Front Matter block
+                break
+
+        logger.log(5, "%s raw Front Matter: %s" % (LOG_PREFIX, front_matter_text))
+
+        # Use YAML to read front matter
+        front_matter_joined_test = "\n".join(front_matter_text)
+        metadata = yaml.load(front_matter_joined_test, Loader=yaml.Loader)
+
+        if metadata is None:
+            metadata = {}
+
+        for k, v in metadata.copy().items():
+            if k in formatted_fields:
+                # formatted metadata is special case and join all list
+                # values
+                if isinstance(v, list | set):
+                    formatted_values = "\n".join(v)
+                else:
+                    formatted_values = v
+                formatted = md.render(formatted_values)
+                # drop final newline
+                formatted = formatted.strip()
+                # if anything but "summary", drop it out of the <p> tag
+                if (
+                    k != "summary"
+                    and formatted.startswith("<p>")
+                    and formatted.endswith("</p>")
+                ):
+                    formatted = formatted[3:-4].strip()
+                metadata[k] = self.process_metadata(k, formatted)
+            # elif not DUPLICATES_DEFINITIONS_ALLOWED.get(k, True):
+            #     if len(v) > 1:
+            #         logger.warning(
+            #             '%s Duplicate definition of "%s" '
+            #             'for "%s". Using first one.',
+            #             LOG_PREFIX,
+            #             k,
+            #             filename,
+            #         )
+            #     metadata[k] = self.process_metadata(k, v[0])
+            # elif len(v) > 1:
+            #     # handle list metadata as list of string
+            #     metadata[k] = self.process_metadata(k, v)
+            else:
+                # otherwise, handle metadata as single string
+                # metadata[k] = self.process_metadata(k, v[0])
+                metadata[k] = self.process_metadata(k, v)
+
+        md_base_content = raw_text
+
+    else:
+        # This is kept here, and is assumed to work, but not well tested. Use
+        # with caution.
+        front_matter_text = list(raw_text.splitlines())
+
+        for i, line in enumerate(front_matter_text):
+            kv = line.split(":", 1)
+            if len(kv) == 2:
+                name, value = kv[0], kv[1]
+                name = name.lower().strip()
+                value = value.strip()
+                # value = value.split(",")
+                # logger.warning("%s %s %s %s" % (LOG_PREFIX, filename, name, value))
+                # print(i, line, kv, name, value)
+
+                if name in formatted_fields:
+                    # formatted metadata is special case and join all list
+                    # values
+                    if isinstance(value, list | set):
+                        formatted_values = "\n".join(value)
+                    else:
+                        formatted_values = value
+                    formatted = md.render(formatted_values)
+                    # drop final newline
+                    formatted = formatted.strip()
+                    # if "title", drop it out of the <p> tag
+                    if (
+                        name == "title"
+                        and formatted.startswith("<p>")
+                        and formatted.endswith("</p>")
+                    ):
+                        formatted = formatted[3:-4].strip()
+                    metadata[name] = self.process_metadata(name, formatted)
+                # elif not DUPLICATES_DEFINITIONS_ALLOWED.get(name, True):
+                #     if len(value) > 1:
+                #         logger.warning(
+                #             '%s Duplicate definition of "%s" '
+                #             'for "%s". Using first one.',
+                #             LOG_PREFIX,
+                #             name,
+                #             filename,
+                #         )
+                #     metadata[name] = self.process_metadata(name, value[0])
+                # elif len(value) > 1:
+                #     # handle list metadata as list of string
+                #     metadata[name] = self.process_metadata(name, value)
+                else:
+                    # otherwise, handle metadata as single string
+                    # metadata[name] = self.process_metadata(name, value[0])
+                    metadata[name] = self.process_metadata(name, value)
+            else:
+                # after first line that isn't in key:value format, dump the
+                # rest into "content"
+                content = "\n".join(raw_text[i:])
+                break
+
+        md_base_content = content
+
+    logging.log(5, "%s processed Metadata: %s" % (LOG_PREFIX, metadata))
+
+    # if using Pelican-style (parsed here) front matter, just render the
+    # "body" in markdown. If we're using the Markdown-IT front-matter
+    # plugin, we render the whole thing as it won't print the front matter.
+    return md_base_content, metadata
+
+
+def remove_tag_only_lines(self, raw_text, metadata):
+    """
+    Read front matter and split from main body text.
+
+    Args:
+        self (pelican.reader.BaseReader): used to pull settings
+        raw_text (str): Raw, unprocessed source text (likely in raw Markdown)
+
+    Returns:
+        content (str): Raw source text, now without tags
+        metadata (dict): Metadata of the source text.
+    """
+
+    return raw_text, metadata
--- minchin/pelican/readers/commonmark/reader.py
+++ minchin/pelican/readers/commonmark/reader.py
@@ -1,18 +1,37 @@
+from copy import copy
 import logging
 
 from bs4 import BeautifulSoup
 from markdown_it import MarkdownIt
 
-from pelican.readers import DUPLICATES_DEFINITIONS_ALLOWED, BaseReader
+from pelican.readers import (
+    DUPLICATES_DEFINITIONS_ALLOWED,
+    METADATA_PROCESSORS,
+    BaseReader,
+)
 from pelican.utils import pelican_open
 
 from .constants import LOG_PREFIX
-from .front_matter import read_front_matter
 from .markdown import render_fence, render_image, render_link_open
 from .post_process import h1_as_title, remove_duplicate_h1
-from .reader_utils import get_file_extensions, load_enables, load_extensions
+from .pre_process import read_front_matter, remove_tag_only_lines
+from .reader_utils import (
+    clean_authors,
+    clean_dates,
+    clean_tags,
+    get_file_extensions,
+    load_enables,
+    load_extensions,
+)
 
 logger = logging.getLogger(__name__)
+
+# use custom date cleaner
+METADATA_PROCESSORS_MDIT = METADATA_PROCESSORS.copy()
+METADATA_PROCESSORS_MDIT["date"] = clean_dates
+METADATA_PROCESSORS_MDIT["modified"] = clean_dates
+METADATA_PROCESSORS_MDIT["tags"] = clean_tags
+METADATA_PROCESSORS_MDIT["authors"] = clean_authors
 
 
 class MDITReader(BaseReader):
@@ -41,7 +60,14 @@ class MDITReader(BaseReader):
             # text = list(fp.splitlines())
             raw_text = fp
 
-        content, metadata = read_front_matter(self, raw_text, md)
+        # content, tag_list = remove_tag_only_lines(self, raw_text, dict())
+        content, metadata = read_front_matter(
+            self=self,
+            raw_text=raw_text,
+            # metadata=copy(metadata),
+            metadata=dict(),
+            md=md,
+        )
 
         # add path to metadata
         metadata["path"] = filename
@@ -51,10 +77,32 @@ class MDITReader(BaseReader):
         html_content, metadata = h1_as_title(html_content, metadata, self.settings)
         html_content = remove_duplicate_h1(html_content, metadata, self.settings)
 
-        # print(metadata)
-        # print(md_content)
-        # print()
         return html_content, metadata
+
+    def process_metadata(self, name, value):
+        # here because we need to handle dates, passed to us as dates
+        # also, lowercase key name for processing
+
+        if name.lower() in METADATA_PROCESSORS_MDIT:
+            value_2 = METADATA_PROCESSORS_MDIT[name.lower()](value, self.settings)
+        else:
+            value_2 = value
+
+        logger.log(
+            5,
+            '%s process metadata: "%s": "%s" %s --> "%s" %s / %s'
+            % (
+                LOG_PREFIX,
+                name,
+                value,
+                type(value),
+                value_2,
+                type(value_2),
+                name in METADATA_PROCESSORS_MDIT,
+            ),
+        )
+
+        return value_2
 
 
 def add_commonmark_reader(readers):
