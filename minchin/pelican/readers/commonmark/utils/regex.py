a0f2c1abdb8a60fd350eb0e77985f93ea1da62a1
MinchinWeb <w_minchin@hotmail.com>
2023-12-31 22:54:16 -0700

    Fix Wikilinks regex

--- minchin/pelican/readers/commonmark/utils/regex.py
+++ minchin/pelican/readers/commonmark/utils/regex.py
@@ -21,6 +21,8 @@ def tag_only_line_regex(tag_symbols):
     return re.compile(pattern)
 
 
-_base_wikilink_regex = r"\[\[\s*(?P<filename>[^|\]]+)(\|\s*(?P<linkname>.+))?\]\]"
+_base_wikilink_regex = (
+    r"\[\[\s*(?P<filename>[^|\]]+)(\|\s*(?P<linkname>[^|\]\n\r]+))?\]\]"
+)
 wikilink_regex = re.compile(_base_wikilink_regex)
 wikilink_file_regex = re.compile(r"!" + _base_wikilink_regex)
